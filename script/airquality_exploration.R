5d110fab4b402503df0557ed366e5f90541c1491
ericofrs <ericoqv@gmail.com>
2025-03-07 14:19:55 +0100

    Part 3 (Data cleaning and Transformation) done

--- script/airquality_exploration.R
+++ script/airquality_exploration.R
@@ -18,4 +18,19 @@ str(airquality)
 airqual <- airquality
 
 
+# PART 3
 
+summary(airqual)
+## We see that there are 37 NAs in Ozone and 7 NAs in Solar.R columns
+
+is.na(airqual)
+
+## Lets clean the data by eliminating the rows with NAs
+airqual <- airqual %>% drop_na()
+
+## It is also interesting to create another column with the dates
+airqual$Date<-as.Date(with(airqual,paste(1973,Month,Day,sep="-")),"%Y-%m-%d")
+head(airqual)
+
+# PART 4
+## Lets try to draw relations between Ozone, Solar.R and Temp
